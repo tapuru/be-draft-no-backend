f78bae8c7dd6c3b57e42a8b252bec563bda5b2a5
tapuru <pogladtapira@gmail.com>
2025-07-25 22:25:02 +0500

    fix: fix default team type

--- src/lib/default-team.const.ts
+++ src/lib/default-team.const.ts
@@ -1,7 +1,9 @@
+import { CHARACTERS_LIST } from "./characters-list.const";
 import type { Team } from "./types";
 
 export const DEFAULT_TEAM: Omit<Team, "name"> = {
   bannedCharacters: [],
   pickedCharacters: [],
   score: 0,
+  availableCharacters: CHARACTERS_LIST,
 };
