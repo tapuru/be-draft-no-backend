ec6c7ec309dad05be7d79e92caaa15b8d6b3fa37
tapuru <pogladtapira@gmail.com>
2025-07-26 17:16:48 +0500

    feat: add styles for choose winner

--- src/components/choose-winner/choose-winner.tsx
+++ src/components/choose-winner/choose-winner.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Flex, Typography } from "antd";
+import { Card, Flex, Typography } from "antd";
 import {
   useActions,
   useSelectPickedMaps,
@@ -7,6 +7,7 @@ import {
 } from "../../model";
 import s from "./choose-winner.module.css";
 import { STAGE } from "../../lib";
+import clsx from "clsx";
 
 export const ChooseWinner = () => {
   const teams = useSelectTeams();
@@ -23,9 +24,9 @@ export const ChooseWinner = () => {
 
   return (
     <Flex justify="center" align="center" style={{ height: "100vh" }}>
-      <Card>
-        <Typography.Title>Карта {round} завершена</Typography.Title>
-        <Typography.Title level={3} style={{ color: "green" }}>
+      <Card className={s.card}>
+        <Typography.Title>Карта {round + 1} завершена</Typography.Title>
+        <Typography.Title level={3} style={{ color: "#52c41a" }}>
           {maps[round].name}
         </Typography.Title>
         <Typography.Title level={3}>
@@ -33,12 +34,12 @@ export const ChooseWinner = () => {
         </Typography.Title>
         <div className={s.winners}>
           {teams.map((t, i) => (
-            <Button
-              className={s.winner}
+            <div
+              className={clsx(s.winner, i === 0 ? s._blue : s._red)}
               onClick={() => handleChooseWinner(i as 0 | 1)}
             >
               {t.name}
-            </Button>
+            </div>
           ))}
         </div>
       </Card>
